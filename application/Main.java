e3ea5b7f0dd9eff7f8eadfc5303e5b2cd9a81e27
unknown <eg2895@gmail.com>
2022-12-21 14:16:02 -0800

    Fixed highlight bug and resize bug

--- application/Main.java
+++ application/Main.java
@@ -275,8 +275,10 @@ public class Main extends Application {
 	private HBox addWPMHBox() {
 	    HBox hbox = new HBox();
 	    hbox.setPadding(new Insets(15, 12, 15, 12));
-	    hbox.setSpacing(10);
+	    hbox.setSpacing(8);
 	    wpmLabel.setFont(Font.font("Helvatica", FontWeight.BOLD, 35));
+	    wpmLabel.setAlignment(Pos.CENTER);
+	    wpmLabel.setPrefWidth(182);
 	    hbox.setAlignment(Pos.CENTER);
 	    hbox.getChildren().addAll(this.wpmLabel);
 
